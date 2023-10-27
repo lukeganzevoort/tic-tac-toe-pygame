0aca28828332ed4be2de578f316c335ee96a0f45
Luke Ganzevoort <13698030+lukeganzevoort@users.noreply.github.com>
2023-10-26 21:29:14 -0400

    moved to class

--- main.py
+++ main.py
@@ -2,87 +2,128 @@ import sys
 
 import pygame
 
-# Initialize Pygame
-pygame.init()
 
-# Constants
-WIDTH, HEIGHT = 400, 400
-GRID_SIZE = 3
-GRID_WIDTH = WIDTH // GRID_SIZE
-GRID_HEIGHT = HEIGHT // GRID_SIZE
-WHITE = (255, 255, 255)
-LINE_COLOR = (0, 0, 0)
+class TicTacToe:
+    def __init__(self):
+        # Initialize Pygame
+        pygame.init()
 
-# Initialize the screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Tic-Tac-Toe")
+        # Constants
+        self.WIDTH, self.HEIGHT = 400, 400
+        self.GRID_SIZE = 3
+        self.GRID_WIDTH = self.WIDTH // self.GRID_SIZE
+        self.GRID_HEIGHT = self.HEIGHT // self.GRID_SIZE
+        self.WHITE = (255, 255, 255)
+        self.LINE_COLOR = (0, 0, 0)
 
-# Game variables
-grid = [['' for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
-player_turn = 'X'
-game_over = False
+        # Initialize the screen
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Tic-Tac-Toe")
 
-# Function to draw the grid
-def draw_grid():
-    for row in range(1, GRID_SIZE):
-        pygame.draw.line(screen, LINE_COLOR, (0, row * GRID_HEIGHT), (WIDTH, row * GRID_HEIGHT), 2)
-        pygame.draw.line(screen, LINE_COLOR, (row * GRID_WIDTH, 0), (row * GRID_WIDTH, HEIGHT), 2)
+        # Game variables
+        self.grid = [["" for _ in range(self.GRID_SIZE)] for _ in range(self.GRID_SIZE)]
+        self.player_turn = "X"
+        self.game_over = False
 
-# Function to draw X and O on the board
-def draw_xo():
-    for row in range(GRID_SIZE):
-        for col in range(GRID_SIZE):
-            if grid[row][col] == 'X':
-                pygame.draw.line(screen, LINE_COLOR, (col * GRID_WIDTH, row * GRID_HEIGHT),
-                                 ((col + 1) * GRID_WIDTH, (row + 1) * GRID_HEIGHT), 2)
-                pygame.draw.line(screen, LINE_COLOR, ((col + 1) * GRID_WIDTH, row * GRID_HEIGHT),
-                                 (col * GRID_WIDTH, (row + 1) * GRID_HEIGHT), 2)
-            elif grid[row][col] == 'O':
-                pygame.draw.circle(screen, LINE_COLOR, (col * GRID_WIDTH + GRID_WIDTH // 2, row * GRID_HEIGHT + GRID_HEIGHT // 2), GRID_WIDTH // 2, 2)
+    # Function to draw the grid
+    def draw_grid(self):
+        for row in range(1, self.GRID_SIZE):
+            pygame.draw.line(
+                self.screen,
+                self.LINE_COLOR,
+                (0, row * self.GRID_HEIGHT),
+                (self.WIDTH, row * self.GRID_HEIGHT),
+                2,
+            )
+            pygame.draw.line(
+                self.screen,
+                self.LINE_COLOR,
+                (row * self.GRID_WIDTH, 0),
+                (row * self.GRID_WIDTH, self.HEIGHT),
+                2,
+            )
 
-# Function to check for a win
-def check_win():
-    for row in range(GRID_SIZE):
-        if grid[row][0] == grid[row][1] == grid[row][2] != '':
-            return grid[row][0]
-    for col in range(GRID_SIZE):
-        if grid[0][col] == grid[1][col] == grid[2][col] != '':
-            return grid[0][col]
-    if grid[0][0] == grid[1][1] == grid[2][2] != '':
-        return grid[0][0]
-    if grid[0][2] == grid[1][1] == grid[2][0] != '':
-        return grid[0][2]
-    return None
+    # Function to draw X and O on the board
+    def draw_xo(self):
+        for row in range(self.GRID_SIZE):
+            for col in range(self.GRID_SIZE):
+                if self.grid[row][col] == "X":
+                    pygame.draw.line(
+                        self.screen,
+                        self.LINE_COLOR,
+                        (col * self.GRID_WIDTH, row * self.GRID_HEIGHT),
+                        ((col + 1) * self.GRID_WIDTH, (row + 1) * self.GRID_HEIGHT),
+                        2,
+                    )
+                    pygame.draw.line(
+                        self.screen,
+                        self.LINE_COLOR,
+                        ((col + 1) * self.GRID_WIDTH, row * self.GRID_HEIGHT),
+                        (col * self.GRID_WIDTH, (row + 1) * self.GRID_HEIGHT),
+                        2,
+                    )
+                elif self.grid[row][col] == "O":
+                    pygame.draw.circle(
+                        self.screen,
+                        self.LINE_COLOR,
+                        (
+                            col * self.GRID_WIDTH + self.GRID_WIDTH // 2,
+                            row * self.GRID_HEIGHT + self.GRID_HEIGHT // 2,
+                        ),
+                        self.GRID_WIDTH // 2,
+                        2,
+                    )
 
-# Main game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    # Function to check for a win
+    def check_win(self):
+        for row in range(self.GRID_SIZE):
+            if self.grid[row][0] == self.grid[row][1] == self.grid[row][2] != "":
+                return self.grid[row][0]
+        for col in range(self.GRID_SIZE):
+            if self.grid[0][col] == self.grid[1][col] == self.grid[2][col] != "":
+                return self.grid[0][col]
+        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] != "":
+            return self.grid[0][0]
+        if self.grid[0][2] == self.grid[1][1] == self.grid[2][0] != "":
+            return self.grid[0][2]
+        return None
 
-        if not game_over:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                col = x // GRID_WIDTH
-                row = y // GRID_HEIGHT
+    # Main game loop
+    def play(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-                if grid[row][col] == '':
-                    grid[row][col] = player_turn
-                    player_turn = 'O' if player_turn == 'X' else 'X'
+                if not self.game_over:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        x, y = event.pos
+                        col = x // self.GRID_WIDTH
+                        row = y // self.GRID_HEIGHT
 
-                winner = check_win()
-                if winner:
-                    game_over = True
+                        if self.grid[row][col] == "":
+                            self.grid[row][col] = self.player_turn
+                            self.player_turn = "O" if self.player_turn == "X" else "X"
 
-    screen.fill(WHITE)
-    draw_grid()
-    draw_xo()
+                        winner = self.check_win()
+                        if winner:
+                            self.game_over = True
 
-    if game_over:
-        font = pygame.font.Font(None, 36)
-        text = font.render(f"Player {winner} wins!", True, LINE_COLOR)
-        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-        screen.blit(text, text_rect)
+            self.screen.fill(self.WHITE)
+            self.draw_grid()
+            self.draw_xo()
 
-    pygame.display.update()
+            if self.game_over:
+                font = pygame.font.Font(None, 36)
+                text = font.render(f"Player {winner} wins!", True, self.LINE_COLOR)
+                text_rect = text.get_rect(center=(self.WIDTH // 2, self.HEIGHT // 2))
+                self.screen.blit(text, text_rect)
+
+            pygame.display.update()
+
+
+# Create an instance of the TicTacToe class and start the game
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.play()
