57c088223a57e45d8d5139918c82843f63c4873c
Luke Ganzevoort <13698030+lukeganzevoort@users.noreply.github.com>
2023-11-01 22:53:52 -0400

    updated type hints

--- game.py
+++ game.py
@@ -1,14 +1,15 @@
 import random
+from typing import Optional
 
 import numpy as np
 
 
 class TicTacToe:
-    def __init__(self):
-        self.board = np.zeros((3, 3), dtype=int)
-        self.current_player = random.choice([1, 2])
+    def __init__(self) -> None:
+        self.board: np.ndarray = np.zeros((3, 3), dtype=int)
+        self.current_player: int = random.choice([1, 2])
 
-    def move(self, row, col):
+    def move(self, row: int, col: int) -> bool:
         if self.board[row, col] == 0 and self.winner() is None:
             self.board[row, col] = self.current_player
             self.current_player = 3 - self.current_player
@@ -16,7 +17,9 @@ class TicTacToe:
         else:
             return False
 
-    def winner(self):
+    def winner(self) -> Optional[int]:
+        if self.is_full():
+            return 0
         for player in [1, 2]:
             for i in range(3):
                 if np.all(self.board[i, :] == player) or np.all(
@@ -29,8 +32,8 @@ class TicTacToe:
                 return player
         return None
 
-    def get_board(self):
+    def get_board(self) -> np.ndarray:
         return self.board
 
-    def is_full(self):
+    def is_full(self) -> bool:
         return not (self.board == 0).any()
