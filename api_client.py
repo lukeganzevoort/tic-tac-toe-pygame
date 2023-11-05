844c23039bdb6e584207ae1ab1b4368a50179fac
Luke Ganzevoort <13698030+lukeganzevoort@users.noreply.github.com>
2023-11-05 13:22:05 -0500

    added logging to api client

--- api_client.py
+++ api_client.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional
 
 import requests
@@ -5,6 +6,8 @@ import requests
 base_url = "http://localhost:5000"
 
 Board = list[list[int]]
+# Configure the logging settings
+logging.basicConfig(level=logging.WARNING)
 
 
 def start_game() -> Optional[str]:
@@ -12,42 +15,58 @@ def start_game() -> Optional[str]:
     if response.status_code == 200:
         data = response.json()
         if not isinstance(user_id := data.get("user_id"), str):
+            logging.warning("Invalid 'user_id' received from the API")
             return None
         return user_id
     else:
+        logging.warning(f"Failed to start a game. Status code: {response.status_code}")
         return None
 
 
 def make_move(user_id: str, row: int, col: int) -> bool:
     payload = {"row": row, "col": col}
     response = requests.post(f"{base_url}/make_move/{user_id}", json=payload)
-    if response.status_code != 200:
+    if response.status_code == 200:
+        _ = response.json().get("current_player")
+        return True
+    else:
+        logging.warning(f"Failed to make a move. Status code: {response.status_code}")
         return False
-    _ = response.json().get("current_player")
-    return True
 
 
 def get_status(user_id: str) -> Optional[tuple[Board, int, int, Optional[int]]]:
     response = requests.get(f"{base_url}/get_status/{user_id}")
     if response.status_code == 200:
         data = response.json()
-        if not isinstance(board := data.get("board"), list):
+        board = data.get("board")
+        current_player = data.get("current_player")
+        your_player_id = data.get("your_player_id")
+        winner = data.get("winner")
+
+        if (
+            not isinstance(board, list)
+            or not isinstance(current_player, int)
+            or not isinstance(your_player_id, int)
+            or (winner is not None and not isinstance(winner, int))
+        ):
+            logging.warning("Invalid data received from the API")
             return None
-        if not isinstance(current_player := data.get("current_player"), int):
-            return None
-        if not isinstance(your_player_id := data.get("your_player_id"), int):
-            return None
-        if not isinstance(winner := data.get("winner"), (int)) and winner is not None:
-            return None
-        return (board, current_player, your_player_id, winner)
+
+        return board, current_player, your_player_id, winner
     else:
+        logging.warning(
+            f"Failed to get game status. Status code: {response.status_code}"
+        )
         return None
 
 
-def is_ready(self, user_id) -> Optional[bool]:
+def is_ready(user_id: str) -> Optional[bool]:
     response = requests.get(f"{base_url}/ready/{user_id}")
     if response.status_code == 200:
         data = response.json()
         return data["ready"]
     else:
+        logging.warning(
+            f"Failed to check readiness. Status code: {response.status_code}"
+        )
         return None
